103aed137a0f155e25a3205cf2e34c4f9fdfa83e
juliendubrail <juliendubrail@users.noreply.github.com>
2019-03-16 12:42:21 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# cineflix
-Created with CodeSandbox
---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div className="SearchBar">
+      <h1>Movies</h1>
+      <input type="search" placeholder="Search Movies ..." />
+    </div>
+  );
+};
+
+export default SearchBar;
