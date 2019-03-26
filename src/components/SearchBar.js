acf849928af93c48b3d5799e7d41a60f0c78c098
juliendubrail <dubrail.julien@gmail.com>
2019-03-27 00:10:30 +0100

    dynamic search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,27 +1,11 @@
-import React from "react";
+import React from 'react';
 
 const SearchBar = ({onInputChange}) => {
-  return (
-    //<div className="SearchBar">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-    <a class="navbar-brand" href="#">Cineflix</a>
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search Movies ..." aria-label="Search"  onChange={onInputChange}/>
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-   // </div>
-  );
-};
+    return(
+        <form className="form-inline my-2 my-lg-0">
+        <input className="form-control mr-sm-2" type="search" placeholder="Search Movies ..." aria-label="Search" onKeyUp={onInputChange}/>
+        </form>
+    );
+}
 
 export default SearchBar;
